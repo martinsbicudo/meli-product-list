2a8ad3c040a040fa581e07800f14dd2b58366d20
Victor Martins Bicudo <tqi_vbicudo@uolinc.com>
2019-08-13 11:23:19 -0300

    #meli-02 - Cria o Details Container - Adiciona o Details Container na página de Detalhes do Produto

--- client/pages/items/[id].jsx
+++ client/pages/items/[id].jsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { string, number, oneOfType } from 'prop-types';
+import Details from '@Containers/Details';
 import Layout from '../_layout';
 
 const Items = ({
   id,
-}) => (
-  <Layout title="TEST_TITLE">
-    Detalhes
-    {id}
-  </Layout>
-);
+}) => {
+  const [title, setTitle] = useState('Carregando produto no Mercado Livro');
+
+  return (
+    <Layout title={title}>
+      <Details setTitle={setTitle} id={id} />
+    </Layout>
+  );
+};
 
 Items.getInitialProps = async ({ query, res }) => {
   if (!query.id) res.redirect('/');
