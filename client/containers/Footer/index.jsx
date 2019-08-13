b37a55b48fe78bab0e1cfe29ae97f2202e40f09e
Victor Martins Bicudo <tqi_vbicudo@uolinc.com>
2019-08-13 06:28:09 -0300

    - Cria o Header Container - Cria o Footer Container - Adiciona imagens da aplicação - Cria o 'assets-transformer' para resolver problema com import de imagens

--- client/containers/Footer/index.jsx
+++ client/containers/Footer/index.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import Container from '@Components/Container';
+import style from './style.scss';
 
 const Footer = () => (
-  <footer>
+  <footer className={style.Footer}>
     <Container>
-      Footer
+      <p className={style.FooterText}>
+        made with love by
+        <a
+          className={style.FooterLink}
+          href="https://github.com/martinsbicudo"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          @martinsbicudo
+        </a>
+      </p>
     </Container>
   </footer>
 );
--- client/containers/Header/spec.js
+++ client/containers/Header/spec.js
@@ -1,9 +1,51 @@
-import Header from '.';
+import '@Client/__mocks__/useRouter'; // eslint-disable-line
+import Header from '.'; // eslint-disable-line
 
 describe('Containers - <Header />', () => {
+  const setState = jest.fn();
+  const useStateSpy = jest.spyOn(React, 'useState');
+  useStateSpy.mockImplementation((value) => [value, setState]);
   const createWrapper = () => render(
     <Header />,
   );
+
+  test('Should finds button disabled', () => {
+    const { container } = createWrapper();
+
+    expect(container.querySelector('button[type="submit"]')).toBeDisabled();
+  });
+
+  test('Should finds button active', () => {
+    const { getByPlaceholderText, container } = createWrapper();
+
+    fireEvent.change(getByPlaceholderText('Buscar produto'), {
+      target: {
+        value: 'TEST_VALUE',
+      },
+    });
+
+    expect(container.querySelector('button[type="submit"]')).not.toBeDisabled();
+  });
+
+  test('Should finds button disabled after submit', () => {
+    const { getByPlaceholderText, container } = createWrapper();
+    const form = container.querySelector('form');
+
+    fireEvent.change(getByPlaceholderText('Buscar produto'), {
+      target: {
+        value: 'TEST_VALUE',
+      },
+    });
+    fireEvent.submit(form);
+
+    expect(container.querySelector('button[type="submit"]')).toBeDisabled();
+  });
+
+  test('Simulatw focus', () => {
+    const { getByPlaceholderText } = createWrapper();
+
+    fireEvent.focus(getByPlaceholderText('Buscar produto'));
+  });
 
   test('Wrapper Snapshot', () => {
     const { asFragment } = createWrapper();
